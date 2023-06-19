fc50c4060b55419f1c1a49e26353e7434845ec35
agustin cordero <agustin.cordero201@gmail.com>
2023-06-18 21:15:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [number, setNumber] = useState(0)
+
+  const changeNumber = () =>{
+    setNumber('2')
+  }
+
+  return (
+    <div className='App'>
+      {number}
+      <button onClick={changeNumber}>Change number</button>
+    </div>
+  )
+}
+
+export default App
